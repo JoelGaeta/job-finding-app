f1e3fbc47229a6ce65613e3aab3ddf9d69087629
Bryan Hadaway <treatmentrecordsolutions@gmail.com>
2020-10-16 12:25:26 -0400

    commit

--- components/ProfileViewImages.js
+++ components/ProfileViewImages.js
@@ -8,7 +8,19 @@ const ProfileViewImages = () => {
             <Image style={styles.images} source={{ uri: "https://i.pinimg.com/originals/ea/5c/07/ea5c0756f5c2980e8acecf61f52a61fd.jpg", }} />
             <Text>I love dancing, acting, and hanging out with friends! a;sdlkfjas;ldfjals;dkjfas;ldkfjasl;dkfjas;ldfjk;asdkfjas;dfjkas;dfkja;lsdkfjas;ldfkja;sldkfja;sdkfja;sdkfja;sdfjk</Text>
             <Text>I love dancing, acting, and hanging out with friends! a;sdlkfjas;ldfjals;dkjfas;ldkfjasl;dkfjas;ldfjk;asdkfjas;dfjkas;dfkja;lsdkfjas;ldfkja;sldkfja;sdkfja;sdkfja;sdfjk</Text>
+            <Image style={styles.images} source={{ uri: "https://www.spidermancrawlspace.com/wp-content/uploads/2019/07/3131.jpg", }} />
+            <Text>I love dancing, acting, and hanging out with friends! a;sdlkfjas;ldfjals;dkjfas;ldkfjasl;dkfjas;ldfjk;asdkfjas;dfjkas;dfkja;lsdkfjas;ldfkja;sldkfja;sdkfja;sdkfja;sdfjk</Text>
+            <Text>I love dancing, acting, and hanging out with friends! a;sdlkfjas;ldfjals;dkjfas;ldkfjasl;dkfjas;ldfjk;asdkfjas;dfjkas;dfkja;lsdkfjas;ldfkja;sldkfja;sdkfja;sdkfja;sdfjk</Text>
+            <Image style={styles.images} source={{ uri: "https://www.spidermancrawlspace.com/wp-content/uploads/2019/07/002SDM_Kirsten_Dunst_010.jpg", }} />
+            <Text>I love dancing, acting, and hanging out with friends! a;sdlkfjas;ldfjals;dkjfas;ldkfjasl;dkfjas;ldfjk;asdkfjas;dfjkas;dfkja;lsdkfjas;ldfkja;sldkfja;sdkfja;sdkfja;sdfjk</Text>
+            <Text>I love dancing, acting, and hanging out with friends! a;sdlkfjas;ldfjals;dkjfas;ldkfjasl;dkfjas;ldfjk;asdkfjas;dfjkas;dfkja;lsdkfjas;ldfkja;sldkfja;sdkfja;sdkfja;sdfjk</Text>
+            <Image style={styles.images} source={{ uri: "https://hips.hearstapps.com/digitalspyuk.cdnds.net/17/20/1495016266-mary-jane-watson-kirsten-dunst.jpg?resize=480:*", }} />
+            <Text>I love dancing, acting, and hanging out with friends! a;sdlkfjas;ldfjals;dkjfas;ldkfjasl;dkfjas;ldfjk;asdkfjas;dfjkas;dfkja;lsdkfjas;ldfkja;sldkfja;sdkfja;sdkfja;sdfjk</Text>
+            <Text>I love dancing, acting, and hanging out with friends! a;sdlkfjas;ldfjals;dkjfas;ldkfjasl;dkfjas;ldfjk;asdkfjas;dfjkas;dfkja;lsdkfjas;ldfkja;sldkfja;sdkfja;sdkfja;sdfjk</Text>
             <Image style={styles.images} source={{ uri: "https://img-9gag-fun.9cache.com/photo/aAgY6d0_460s.jpg", }} />
+            <Text>I love dancing, acting, and hanging out with friends! a;sdlkfjas;ldfjals;dkjfas;ldkfjasl;dkfjas;ldfjk;asdkfjas;dfjkas;dfkja;lsdkfjas;ldfkja;sldkfja;sdkfja;sdkfja;sdfjk</Text>
+            <Text>I love dancing, acting, and hanging out with friends! a;sdlkfjas;ldfjals;dkjfas;ldkfjasl;dkfjas;ldfjk;asdkfjas;dfjkas;dfkja;lsdkfjas;ldfkja;sldkfja;sdkfja;sdkfja;sdfjk</Text>
+            <Image style={styles.images} source={{ uri: "https://pm1.narvii.com/6504/c00ee287b49cb954ea95876046d11c875dd5c5b5_00.jpg", }} />
             <Text>I love dancing, acting, and hanging out with friends! a;sdlkfjas;ldfjals;dkjfas;ldkfjasl;dkfjas;ldfjk;asdkfjas;dfjkas;dfkja;lsdkfjas;ldfkja;sldkfja;sdkfja;sdkfja;sdfjk</Text>
             <Text>I love dancing, acting, and hanging out with friends! a;sdlkfjas;ldfjals;dkjfas;ldkfjasl;dkfjas;ldfjk;asdkfjas;dfjkas;dfkja;lsdkfjas;ldfkja;sldkfja;sdkfja;sdkfja;sdfjk</Text>
         </View>
--- modals/ProfileEdit.js
+++ modals/ProfileEdit.js
@@ -3,6 +3,14 @@ import { StyleSheet, Text, Image, View, Modal, ScrollView, Alert } from "react-n
 import { Button } from 'react-native-paper';
 import ModalButtons from '../components/ModalButtons'
 const ProfileEdit = ({ profileEditModalVisible, setProfileEditModalVisible }) => {
+    const [profilePictures] = useState([
+        { img: "https://i.pinimg.com/originals/ea/5c/07/ea5c0756f5c2980e8acecf61f52a61fd.jpg" },
+        { img: "https://www.spidermancrawlspace.com/wp-content/uploads/2019/07/3131.jpg" },
+        { img: "https://img-9gag-fun.9cache.com/photo/aAgY6d0_460s.jpg" },
+        { img: "https://www.spidermancrawlspace.com/wp-content/uploads/2019/07/002SDM_Kirsten_Dunst_010.jpg" },
+        { img: "https://hips.hearstapps.com/digitalspyuk.cdnds.net/17/20/1495016266-mary-jane-watson-kirsten-dunst.jpg?resize=480:*" },
+        { img: "https://pm1.narvii.com/6504/c00ee287b49cb954ea95876046d11c875dd5c5b5_00.jpg" }
+    ])
     return (
         <Modal animationType="slide" visible={profileEditModalVisible}>
             <View style={styles.header}>
@@ -21,7 +29,7 @@ const ProfileEdit = ({ profileEditModalVisible, setProfileEditModalVisible }) =>
                         <View style={styles.imageContainer}>
                             <Image
                                 source={{
-                                    uri: "https://i.pinimg.com/originals/ea/5c/07/ea5c0756f5c2980e8acecf61f52a61fd.jpg",
+                                    uri: profilePictures[0].img,
                                 }}
                                 style={styles.images}
                             />
@@ -29,16 +37,7 @@ const ProfileEdit = ({ profileEditModalVisible, setProfileEditModalVisible }) =>
                         <View style={styles.imageContainer}>
                             <Image
                                 source={{
-                                    uri: "https://i.pinimg.com/originals/ea/5c/07/ea5c0756f5c2980e8acecf61f52a61fd.jpg",
-                                }}
-                                style={styles.images}
-                            />
-                        </View>
-
-                        <View style={styles.imageContainer}>
-                            <Image
-                                source={{
-                                    uri: "https://i.pinimg.com/originals/ea/5c/07/ea5c0756f5c2980e8acecf61f52a61fd.jpg",
+                                    uri: profilePictures[1].img,
                                 }}
                                 style={styles.images}
                             />
@@ -47,7 +46,7 @@ const ProfileEdit = ({ profileEditModalVisible, setProfileEditModalVisible }) =>
                         <View style={styles.imageContainer}>
                             <Image
                                 source={{
-                                    uri: "https://i.pinimg.com/originals/ea/5c/07/ea5c0756f5c2980e8acecf61f52a61fd.jpg",
+                                    uri: profilePictures[2].img,
                                 }}
                                 style={styles.images}
                             />
@@ -56,7 +55,7 @@ const ProfileEdit = ({ profileEditModalVisible, setProfileEditModalVisible }) =>
                         <View style={styles.imageContainer}>
                             <Image
                                 source={{
-                                    uri: "https://i.pinimg.com/originals/ea/5c/07/ea5c0756f5c2980e8acecf61f52a61fd.jpg",
+                                    uri: profilePictures[3].img,
                                 }}
                                 style={styles.images}
                             />
@@ -65,7 +64,16 @@ const ProfileEdit = ({ profileEditModalVisible, setProfileEditModalVisible }) =>
                         <View style={styles.imageContainer}>
                             <Image
                                 source={{
-                                    uri: "https://i.pinimg.com/originals/ea/5c/07/ea5c0756f5c2980e8acecf61f52a61fd.jpg",
+                                    uri: profilePictures[4].img,
+                                }}
+                                style={styles.images}
+                            />
+                        </View>
+
+                        <View style={styles.imageContainer}>
+                            <Image
+                                source={{
+                                    uri: profilePictures[5].img,
                                 }}
                                 style={styles.images}
                             />
